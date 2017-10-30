e1812716f3a3733f5ac7897b20d1af79fad1242a
Daryl Lukas <me@daryllukas.co>
2017-10-30 10:12:27 +0200

    Updated UI. Fixed authentication

--- CampusGo/urls.py
+++ CampusGo/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from web import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
     url(r'^register/$', views.register, name='register'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^search/$', views.search, name='search'),
     url(r'^location/json/$', views.location_json, name='location_json'),
 
